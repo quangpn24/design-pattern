2325ac1727aeac5397a7754ce913cf79c363d578
anhtt2211 <trananh.2211.work@gmail.com>
2023-07-08 23:41:26 +0700

    upload decorator pattern

---
+++ DecoratorPattern/Decorator/ConcreteDecorator.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Decorator
+{
+    public class DieselCarDecorator : CarDecorator
+    {
+        public DieselCarDecorator(ICar car) : base(car)
+        {
+        }
+        public override ICar ManufactureCar()
+        {
+            car.ManufactureCar();
+            AddEngine(car);
+            return car;
+        }
+        public void AddEngine(ICar car)
+        {
+            if (car is BMWCar)
+            {
+                BMWCar BMWCar = (BMWCar)car;
+                BMWCar.Engine = "Diesel Engine";
+                Console.WriteLine("DieselCarDecorator added Diesel Engine to the Car : " + car);
+            }
+        }
+    }
+
+    class PetrolCarDecorator : CarDecorator
+    {
+        public PetrolCarDecorator(ICar car) : base(car)
+        {
+        }
+        public override ICar ManufactureCar()
+        {
+            car.ManufactureCar();
+            AddEngine(car);
+            return car;
+        }
+        public void AddEngine(ICar car)
+        {
+            if (car is BMWCar)
+            {
+                BMWCar BMWCar = (BMWCar)car;
+                BMWCar.Engine = "Petrol Engine";
+                Console.WriteLine("PetrolCarDecorator added Petrol Engine to the Car : " + car);
+            }
+        }
+    }
+
+}
